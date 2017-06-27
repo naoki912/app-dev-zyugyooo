d0647f6ccbc1f384358118df51ea71afe74cd390
Naoki Kanazawa <kanazawa@naoki912.net>
2017-06-28 05:32:24 +0900

    Implement MyFirstApp

--- android/MyFirstApp/app/src/main/java/soft/hnron/myfirstapp/MainActivityFragment.kt
+++ android/MyFirstApp/app/src/main/java/soft/hnron/myfirstapp/MainActivityFragment.kt
@@ -5,14 +5,27 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 
-/**
- * A placeholder fragment containing a simple view.
- */
+
 class MainActivityFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        if (view != null) {
+            val textView = view.findViewById<TextView>(R.id.text_view)
+            val button = view.findViewById<Button>(R.id.button)
+
+            button.setOnClickListener { view ->
+                textView.text = resources.getString(R.string.label_tapped_text)
+            }
+        }
     }
 }
