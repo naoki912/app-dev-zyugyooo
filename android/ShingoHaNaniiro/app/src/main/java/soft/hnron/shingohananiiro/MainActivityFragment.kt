539a7bb3d02ac481b06c9a5b6e06971f44cacfd2
Naoki Kanazawa <kanazawa@naoki912.net>
2017-06-28 21:06:32 +0900

    Enabled kotlin in ShingoHaNaniiro project

---
+++ android/ShingoHaNaniiro/app/src/main/java/soft/hnron/shingohananiiro/MainActivityFragment.kt
@@ -0,0 +1,16 @@
+package soft.hnron.shingohananiiro
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class MainActivityFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+}
