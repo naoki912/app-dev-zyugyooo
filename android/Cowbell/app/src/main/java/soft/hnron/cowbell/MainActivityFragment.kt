3a4dd6962db89130e54833be360052f89885dd67
Naoki Kanazawa <kanazawa@naoki912.net>
2017-06-30 07:22:27 +0900

    Implement Cowbell

--- android/Cowbell/app/src/main/java/soft/hnron/cowbell/MainActivityFragment.kt
+++ android/Cowbell/app/src/main/java/soft/hnron/cowbell/MainActivityFragment.kt
@@ -1,15 +1,35 @@
 package soft.hnron.cowbell
 
+import android.media.MediaPlayer
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 
 class MainActivityFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val mediaPlayer = MediaPlayer.create(context, R.raw.cowbell)
+
+        val imageButton = view?.findViewById<ImageButton>(R.id.imageButton)
+        imageButton?.setOnClickListener { v: View? ->
+
+            // 音楽ファイルの再生時間が2〜3秒ほどあるため、
+            // 一度seekを0にしないと連打した時に音がならない
+            if (mediaPlayer.isPlaying) {
+                mediaPlayer.seekTo(0)
+            }
+
+            mediaPlayer.start()
+        }
     }
 }
