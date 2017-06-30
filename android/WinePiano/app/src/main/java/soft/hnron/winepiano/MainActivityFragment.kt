be3216876397a3aa1d9c781d72fd43fa325ed9f8
Naoki Kanazawa <kanazawa@naoki912.net>
2017-07-01 07:58:36 +0900

    [Add] WinePiano: デフォで用意されているアニメーションのみを適応

--- android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivityFragment.kt
+++ android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivityFragment.kt
@@ -7,7 +7,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.ScaleAnimation
 import android.widget.ImageButton
+import android.support.v4.content.res.ResourcesCompat
+import android.view.animation.RotateAnimation
+import android.view.animation.TranslateAnimation
 
 class MainActivityFragment : Fragment() {
 
@@ -41,24 +45,48 @@ class MainActivityFragment : Fragment() {
             mGlassPlayer.start()
         }
 
+        val wineImage = ResourcesCompat.getDrawable(resources, R.drawable.wine, null)!!
+
+        val rotateAnimation = RotateAnimation(
+                0f,
+                360f,
+                wineImage.intrinsicWidth.toFloat() / 2,
+                wineImage.intrinsicHeight.toFloat() / 2
+        )
+        rotateAnimation.duration = 500
+
+        val translateAnimation = TranslateAnimation(0f, 0f, 0f, -100f)
+        translateAnimation.duration = 500
+
+        val scaleUpAnimation = ScaleAnimation(1.0f, 1.5f, 1.0f, 1.5f)
+        scaleUpAnimation.duration = 500
+
+        val scaleDownAnimation = ScaleAnimation(1.0f, 0.5f, 1.0f, 0.5f)
+        scaleDownAnimation.duration = 500
+
         bottomRightGlassButton?.setOnClickListener {
             playGlassSound("android.resource://soft.hnron.winepiano/raw/bottom_right")
+            // 反転
         }
 
         bottomCenterGlassButton?.setOnClickListener {
             playGlassSound("android.resource://soft.hnron.winepiano/raw/bottom_center")
+            bottomCenterGlassButton.startAnimation(rotateAnimation)
         }
 
         bottomLeftGlassButton?.setOnClickListener {
             playGlassSound("android.resource://soft.hnron.winepiano/raw/bottom_left")
+            bottomLeftGlassButton.startAnimation(translateAnimation)
         }
 
         upperRightGlassButton?.setOnClickListener {
             playGlassSound("android.resource://soft.hnron.winepiano/raw/upper_right")
+            upperRightGlassButton.startAnimation(scaleDownAnimation)
         }
 
         upperLeftGlassButton?.setOnClickListener {
             playGlassSound("android.resource://soft.hnron.winepiano/raw/upper_left")
+            upperLeftGlassButton.startAnimation(scaleUpAnimation)
         }
     }
 
