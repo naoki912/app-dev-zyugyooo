8425df7a512777377ff6c2d8700209a94f81ba49
Naoki Kanazawa <kanazawa@naoki912.net>
2017-07-01 05:04:06 +0900

    WinePiano: 一部の実装
    フルスクリーン化
    横画面固定
    foreground時のみBGMが流れるようにした
    UI

--- android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivity.kt
+++ android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivity.kt
@@ -2,11 +2,25 @@ package soft.hnron.winepiano
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if (hasFocus) {
+            window.decorView.systemUiVisibility =
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                            View.SYSTEM_UI_FLAG_FULLSCREEN or
+                            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        }
     }
 }
--- android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivityFragment.kt
+++ android/WinePiano/app/src/main/java/soft/hnron/winepiano/MainActivityFragment.kt
@@ -1,5 +1,7 @@
 package soft.hnron.winepiano
 
+import android.media.MediaPlayer
+import android.net.Uri
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,8 +10,31 @@ import android.view.ViewGroup
 
 class MainActivityFragment : Fragment() {
 
+    private var mPlayer = MediaPlayer()
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mPlayer.setDataSource(context, Uri.parse("android.resource://soft.hnron.winepiano/raw/bgm"))
+        mPlayer.prepare()
+        mPlayer.isLooping = true
+        mPlayer.start()
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        mPlayer.pause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mPlayer.start()
     }
 }
