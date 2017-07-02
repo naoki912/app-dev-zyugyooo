fac37e319d839df52948b51a8e18c52f23b017df
Naoki Kanazawa <kanazawa@naoki912.net>
2017-07-03 03:52:10 +0900

    [Update] VeseKentei: スタートボタンをタップするとKenteiFragmentに遷移するように変更
    content_main.xmlの削除
    XMLで<Fragment>を使わずに、containerを用意してMainActivityでMainActivityFragmentを生成するように変更
    KenteiFragmentをリファクタリング
    これらに合わせてMainActivityとMainActivityFragmentを変更

--- android/VeseKentei/app/src/main/java/soft/hnron/vesekentei/MainActivity.kt
+++ android/VeseKentei/app/src/main/java/soft/hnron/vesekentei/MainActivity.kt
@@ -3,10 +3,25 @@ package soft.hnron.vesekentei
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),
+        MainActivityFragment.MainActivityFragmentListener,
+        KenteiFragment.KenteiFragmentListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container, MainActivityFragment.newInstance())
+                .commit()
+    }
+
+    override fun onStartButtonClick() {
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container, KenteiFragment.newInstance(), KenteiFragment.TAG)
+                .addToBackStack(KenteiFragment.TAG)
+                .commit()
     }
 }
--- android/VeseKentei/app/src/main/java/soft/hnron/vesekentei/MainActivityFragment.kt
+++ android/VeseKentei/app/src/main/java/soft/hnron/vesekentei/MainActivityFragment.kt
@@ -1,15 +1,51 @@
 package soft.hnron.vesekentei
 
+import android.content.Context
 import android.support.v4.app.Fragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 class MainActivityFragment : Fragment() {
+
+    private var mListener: MainActivityFragmentListener? = null
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         return inflater!!.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val startButton = view?.findViewById<Button>(R.id.fragment_main__button__start)
+
+        startButton?.setOnClickListener {
+            mListener?.onStartButtonClick()
+        }
+    }
+
+    override fun onAttach(context: Context?) {
+        super.onAttach(context)
+        if (context is MainActivityFragmentListener) {
+            mListener = context
+        } else {
+            throw RuntimeException(context!!.toString() + " must implement MainActivityFragmentListener")
+        }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        mListener = null
+    }
+
+    interface MainActivityFragmentListener {
+        fun onStartButtonClick()
+    }
+
+    companion object {
+        fun newInstance(): MainActivityFragment = MainActivityFragment()
     }
 }
